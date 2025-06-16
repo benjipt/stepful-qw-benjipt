6ecac3207fd536269b8d346d2cf1af92094958b8
Benji Thompson <thompson.benji@gmail.com>
2025-06-16 17:38:36 -0400

    chore[FE]: use env FE port in vite config during startup and setup proxy to api/

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  // Load all env variables, including VITE_*
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      port: Number(env.VITE_FRONTEND_PORT) || 5173,
+      // The proxy option is used to forward API requests from the frontend dev server to the backend server during development.
+      // For example, requests to /api/* will be proxied to the backend, avoiding CORS issues.
+      proxy: {
+        '/api': {
+          target: `http://localhost:${env.VITE_BACKEND_PORT ?? 3000}`,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
+});
