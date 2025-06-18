6d3add21415a7fc2db3b41367f08359cae07bd90
Benji Thompson <thompson.benji@gmail.com>
2025-06-18 14:06:12 -0400

    chore[FE]: generate api types for frontend use

--- backend/spec/requests/api/user_assignments_spec.rb
+++ backend/spec/requests/api/user_assignments_spec.rb
@@ -5,7 +5,20 @@ RSpec.describe 'api/user_assignments', type: :request do
     get('list user_assignments') do
       parameter name: :userEmail, in: :query, type: :string, description: 'User email', required: true
 
+      produces 'application/json'
       response(200, 'successful') do
+        schema type: :array, items: {
+          type: :object,
+          properties: {
+            assignmentId: { type: :integer },
+            title: { type: :string },
+            status: { type: :string },
+            startedAt: { type: :string, format: :date_time, nullable: true },
+            completedAt: { type: :string, format: :date_time, nullable: true },
+            score: { type: :integer, nullable: true }
+          },
+          required: %w[assignmentId title status]
+        }
         let(:userEmail) { users(:one).email }
 
         after do |example|
--- backend/spec/requests/api/users_spec.rb
+++ backend/spec/requests/api/users_spec.rb
@@ -3,7 +3,18 @@ require 'swagger_helper'
 RSpec.describe 'api/users', type: :request do
   path '/api/users' do
     get('list users') do
+      produces 'application/json'
       response(200, 'successful') do
+        schema type: :array, items: {
+          type: :object,
+          properties: {
+            id: { type: :integer },
+            name: { type: :string },
+            email: { type: :string }
+          },
+          required: %w[id name email]
+        }
+
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
