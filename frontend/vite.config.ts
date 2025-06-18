fde5367326895db6a1db49119d251a0cc8fb634f
Benji Thompson <thompson.benji@gmail.com>
2025-06-18 15:21:19 -0400

    feat[FE]: configure Tanstack Router in project

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
 
@@ -7,7 +8,13 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [
+      tanstackRouter({
+        target: 'react',
+        autoCodeSplitting: true,
+      }),
+      react(),
+      tailwindcss()],
     server: {
       port: Number(env.VITE_FRONTEND_PORT) || 5173,
     }
