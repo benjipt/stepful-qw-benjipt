08244ea057f404dc24d9509dd0f0a453857b6724
Benji Thompson <thompson.benji@gmail.com>
2025-06-17 22:56:16 -0400

    feat[BE]: add users association to assignment

--- backend/app/models/assignment.rb
+++ backend/app/models/assignment.rb
@@ -1,6 +1,7 @@
 class Assignment < ApplicationRecord
   has_many :assignment_questions, dependent: :destroy, inverse_of: :assignment
   has_many :user_assignments, dependent: :destroy, inverse_of: :assignment
+  has_many :users, through: :user_assignments
 
   validate :total_points_must_be_100
 
