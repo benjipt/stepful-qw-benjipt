a446d179c51085ec434549f08166b21721c5a5a3
Benji Thompson <thompson.benji@gmail.com>
2025-06-17 20:44:15 -0400

    fix[BE/test]: update the api_users_index_url helper -> api_users_url

--- backend/test/controllers/api/users_controller_test.rb
+++ backend/test/controllers/api/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class Api::UsersControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
-    get api_users_index_url
+    get api_users_url
     assert_response :success
   end
 end
