e27dc1c0303b7cbb1a3cd5acc3b755329dcacf60
Benji Thompson <thompson.benji@gmail.com>
2025-06-18 18:21:23 -0400

    feat[FE]: render placeholder users data

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,13 +1,23 @@
 import { createFileRoute } from '@tanstack/react-router';
 
+import { loadUsers } from '@/lib/loaders';
+
 export const Route = createFileRoute('/')({
+  loader: () => loadUsers(),
   component: Index,
 });
 
 function Index() {
+  const users = Route.useLoaderData();
+
   return (
     <div className='p-2'>
-      <h3>Welcome Home!</h3>
+      {users.map(user => (
+        <div key={user.id} className='p-2 border-b'>
+          <h2 className='text-lg font-bold'>{user.name}</h2>
+          <p className='text-sm text-gray-600'>{user.email}</p>
+        </div>
+      ))}
     </div>
   );
 }
