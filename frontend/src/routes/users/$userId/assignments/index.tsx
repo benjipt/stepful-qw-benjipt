9b4a5cc6a740e020de1935c99237a4998564f6a5
Benji Thompson <thompson.benji@gmail.com>
2025-06-18 22:50:15 -0400

    feat[FE]: render placeholder user assignment cards

--- frontend/src/routes/users/$userId/assignments/index.tsx
+++ frontend/src/routes/users/$userId/assignments/index.tsx
@@ -1,9 +1,47 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { loadUserAssignments } from '@/lib/loaders';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/users/$userId/assignments/')({
-  component: RouteComponent,
+  loader: async ({ params }) =>
+    loadUserAssignments({ userId: parseInt(params.userId, 10) }),
+  component: UserAssignments,
 });
 
-function RouteComponent() {
-  return <div>Hello "/users/$userId/assignments/"!</div>;
+function UserAssignments() {
+  const userAssignments = Route.useLoaderData();
+
+  return (
+    <div className='flex flex-col gap-4 mt-20 px-8 justify-center'>
+      {userAssignments.map((assignment: any) => (
+        <Card key={assignment.assignmentId}>
+          <CardHeader>
+            <CardTitle>{assignment.title}</CardTitle>
+            <CardDescription>Status: {assignment.status}</CardDescription>
+          </CardHeader>
+          <CardContent>
+            {assignment.startedAt && (
+              <div>
+                Started: {new Date(assignment.startedAt).toLocaleString()}
+              </div>
+            )}
+            {assignment.completedAt && (
+              <div>
+                Completed: {new Date(assignment.completedAt).toLocaleString()}
+              </div>
+            )}
+            {assignment.score !== undefined && assignment.score !== null && (
+              <div>Score: {assignment.score}</div>
+            )}
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  );
 }
