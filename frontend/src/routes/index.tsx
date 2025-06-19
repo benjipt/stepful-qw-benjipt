c08719316e1e0794bc73649c61e6cc4c4d19ae73
Benji Thompson <thompson.benji@gmail.com>
2025-06-18 21:03:12 -0400

    refactor[FE]: create a users/ route and redirect index to users for more RESTFUL routing

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,35 +1,7 @@
-import { createFileRoute } from '@tanstack/react-router';
-
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { User, loadUsers } from '@/lib/loaders';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
-  loader: () => loadUsers(),
-  component: Index,
+  loader: () => {
+    throw redirect({ to: '/users' });
+  },
 });
-
-function Index() {
-  const users = Route.useLoaderData();
-
-  return (
-    <div className='flex-1 flex flex-col p-2'>
-      <div className='mt-18 flex-col gap-16'>
-        <h2 className='text-center text-md font-extralight'>
-          Select a User Profile to get started
-        </h2>
-        <div className='flex flex-col gap-4 mt-8 items-center'>
-          {users.map((user: User) => (
-            <Card key={user.id} className='w-sm bg-neutral-50'>
-              <CardHeader>
-                <CardTitle>{user.name}</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p className='text-sm text-neutral-500'>{user.email}</p>
-              </CardContent>
-            </Card>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-}
---
+++ frontend/src/routes/users/index.tsx
@@ -0,0 +1,35 @@
+import { createFileRoute } from '@tanstack/react-router';
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { User, loadUsers } from '@/lib/loaders';
+
+export const Route = createFileRoute('/users/')({
+  loader: () => loadUsers(),
+  component: UsersIndex,
+});
+
+function UsersIndex() {
+  const users = Route.useLoaderData();
+
+  return (
+    <div className='flex-1 flex flex-col p-2'>
+      <div className='mt-18 flex-col gap-16'>
+        <h2 className='text-center text-md font-extralight'>
+          Select a User Profile to get started
+        </h2>
+        <div className='flex flex-col gap-4 mt-8 items-center'>
+          {users.map((user: User) => (
+            <Card key={user.id} className='w-sm bg-neutral-50'>
+              <CardHeader>
+                <CardTitle>{user.name}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p className='text-sm text-neutral-500'>{user.email}</p>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
