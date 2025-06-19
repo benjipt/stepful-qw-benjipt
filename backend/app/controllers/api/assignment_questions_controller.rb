69dd627435d2609afbae277c0cf26e23a60b409c
Benji Thompson <thompson.benji@gmail.com>
2025-06-19 16:41:22 -0400

    fix[BE]: ensure that assignment_id is correctly accessed in the path to avoid 404

--- backend/app/controllers/api/assignment_questions_controller.rb
+++ backend/app/controllers/api/assignment_questions_controller.rb
@@ -1,12 +1,12 @@
 class Api::AssignmentQuestionsController < ApplicationController
   # GET /api/assignments/{assignmentId}/questions
   # Returns all assignment questions for a given assignment
-  # Expects a path parameter `assignmentId` to identify the assignment
+  # Expects a path parameter `assignmentId` or `assignment_id` to identify the assignment
   # Returns an array of AssignmentQuestionSerializer objects
   # Each object contains the details of an assignment question
   # If no assignment is found, returns a 404 error with an appropriate message
   def index
-    assignment_id = params[:assignmentId]
+    assignment_id = params[:assignmentId] || params[:assignment_id]
     assignment = Assignment.find(assignment_id)
     questions = assignment.assignment_questions
     render json: questions, each_serializer: AssignmentQuestionSerializer
