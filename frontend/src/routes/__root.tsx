77e7dfc1c5f63a72dcac500b8d5e5ec17dc6fac7
Benji Thompson <thompson.benji@gmail.com>
2025-06-20 15:07:47 -0400

    feat[FE]: update border style in root layout

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -4,7 +4,7 @@ import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 export const Route = createRootRoute({
   component: () => (
     <div className='flex flex-col min-h-screen'>
-      <div className='flex items-center p-1 justify-center border border-neutral-300 bg-neutral-50 relative'>
+      <div className='flex items-center p-1 justify-center border border-neutral-400 bg-neutral-50 relative'>
         <Link to='/' className='cursor-pointer'>
           <h1 className='text-xl font-bold'>Stepful QuizWizard</h1>
         </Link>
