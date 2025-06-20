14634840671b58004ea05bafff72588567eb4edc
Benji Thompson <thompson.benji@gmail.com>
2025-06-20 17:29:29 -0400

    feat[FE]: return nextQuestionId in loaderData on AssignmentQuestions route

--- frontend/src/routes/users/$userId/assignments/$assignmentId/index.tsx
+++ frontend/src/routes/users/$userId/assignments/$assignmentId/index.tsx
@@ -1,18 +1,30 @@
 import { createFileRoute } from '@tanstack/react-router';
 
-import { loadAssignmentQuestions } from '@/lib/loaders';
+import { AssignmentQuestion, loadAssignmentQuestions } from '@/lib/loaders';
 import QuestionCard from './-components/question-card';
+
+// Helpers ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+const getNextQuestionId = (assignmentQuestions: AssignmentQuestion[]) => {
+  const firstUnanswered = assignmentQuestions.find(q => !q.response);
+  return firstUnanswered?.questionId;
+};
+// <~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Helpers
 
 export const Route = createFileRoute(
   '/users/$userId/assignments/$assignmentId/',
 )({
-  loader: async ({ params }) =>
-    loadAssignmentQuestions({ userAssignmentId: params.assignmentId }),
+  loader: async ({ params }) => {
+    const assignmentQuestions = await loadAssignmentQuestions({
+      userAssignmentId: params.assignmentId,
+    });
+    const nextQuestionId = getNextQuestionId(assignmentQuestions);
+    return { assignmentQuestions, nextQuestionId };
+  },
   component: AssignmentQuestions,
 });
 
 function AssignmentQuestions() {
-  const questions = Route.useLoaderData();
+  const { assignmentQuestions: questions } = Route.useLoaderData();
 
   return (
     <div className='page'>
