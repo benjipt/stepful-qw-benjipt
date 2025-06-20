9fa66df7ce0cd40bbb6e73724cc331830342ed7d
Benji Thompson <thompson.benji@gmail.com>
2025-06-20 15:53:16 -0400

    refactor[FE]: create reusable QuestionCard component and use in AssignmentQuestions route

---
+++ frontend/src/routes/users/$userId/assignments/$assignmentId/-components/question-card.tsx
@@ -0,0 +1,62 @@
+import RenderIf from '@/components/common/render-if';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Label } from '@/components/ui/label';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import React from 'react';
+import FreeTextResponse from './free-text-response';
+
+export interface Props {
+  questionId: number;
+  content: string;
+  choices?: string[] | null;
+  response?: string | null;
+  points: number;
+}
+
+const QuestionCard: React.FC<Props> = ({
+  questionId,
+  content,
+  choices,
+  response,
+  points,
+}) => {
+  const isMultipleChoice = Array.isArray(choices) && choices.length > 0;
+  return (
+    <Card className='w-md'>
+      <CardHeader>
+        <CardTitle className='leading-7'>{content}</CardTitle>
+        <div className='text-sm text-muted-foreground'>
+          {points} point{points !== 1 ? 's' : ''}
+        </div>
+      </CardHeader>
+      <CardContent>
+        <RenderIf condition={isMultipleChoice}>
+          <RadioGroup
+            defaultValue={response ?? ''}
+            className='gap-2'
+            aria-label='Choices'
+          >
+            {choices?.map((choice, idx) => (
+              <div className='flex items-center space-x-3' key={idx}>
+                <RadioGroupItem
+                  value={choice}
+                  id={`q${questionId}-choice${idx}`}
+                  className='cursor-pointer'
+                />
+                <Label htmlFor={`q${questionId}-choice${idx}`}>{choice}</Label>
+              </div>
+            ))}
+          </RadioGroup>
+        </RenderIf>
+        <RenderIf condition={!isMultipleChoice}>
+          <FreeTextResponse
+            questionId={questionId}
+            existingResponse={response ?? undefined}
+          />
+        </RenderIf>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default QuestionCard;
--- frontend/src/routes/users/$userId/assignments/$assignmentId/index.tsx
+++ frontend/src/routes/users/$userId/assignments/$assignmentId/index.tsx
@@ -1,11 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router';
 
-import RenderIf from '@/components/common/render-if';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { Label } from '@/components/ui/label';
-import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { loadAssignmentQuestions } from '@/lib/loaders';
-import FreeTextResponse from './-components/free-text-response';
+import QuestionCard from './-components/question-card';
 
 export const Route = createFileRoute(
   '/users/$userId/assignments/$assignmentId/',
@@ -21,45 +17,17 @@ function AssignmentQuestions() {
   return (
     <div className='page'>
       <div className='flex flex-col gap-4 px-8 items-center'>
-        {questions?.map(question => {
-          const isMultipleChoice =
-            Array.isArray(question.choices) && question.choices.length > 0;
-          return (
-            <Card key={question.questionId} className='w-md'>
-              <CardHeader>
-                <CardTitle className='leading-7'>{question.content}</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <RenderIf condition={isMultipleChoice}>
-                  <RadioGroup
-                    defaultValue={question.response ?? ''}
-                    className='gap-2'
-                    aria-label='Choices'
-                  >
-                    {question.choices?.map((choice, idx) => (
-                      <div className='flex items-center space-x-3' key={idx}>
-                        <RadioGroupItem
-                          value={choice}
-                          id={`q${question.questionId}-choice${idx}`}
-                          className='cursor-pointer'
-                        />
-                        <Label htmlFor={`q${question.questionId}-choice${idx}`}>
-                          {choice}
-                        </Label>
-                      </div>
-                    ))}
-                  </RadioGroup>
-                </RenderIf>
-                <RenderIf condition={!isMultipleChoice}>
-                  <FreeTextResponse
-                    questionId={question.questionId}
-                    existingResponse={question.response ?? undefined}
-                  />
-                </RenderIf>
-              </CardContent>
-            </Card>
-          );
-        })}
+        {questions?.map(
+          ({ questionId, content, choices, response, points }) => (
+            <QuestionCard
+              questionId={questionId}
+              content={content}
+              choices={choices}
+              response={response}
+              points={points}
+            />
+          ),
+        )}
       </div>
     </div>
   );
