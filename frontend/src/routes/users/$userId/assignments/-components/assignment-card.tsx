60ffa0f2d2c9768b34f01b0ea877844f84d1e921
Benji Thompson <thompson.benji@gmail.com>
2025-06-22 18:41:22 -0400

    feat[FE]: update status styles for Assignment Cards

--- frontend/src/routes/users/$userId/assignments/-components/assignment-card.tsx
+++ frontend/src/routes/users/$userId/assignments/-components/assignment-card.tsx
@@ -27,39 +27,50 @@ const AssignmentCard: React.FC<Props> = ({
   totalTimeSpent,
   userId,
   route,
-}) => (
-  <Link
-    to={route}
-    params={{ userId, assignmentId: userAssignmentId.toString() }}
-    key={userAssignmentId}
-    className='w-full max-w-sm lg:max-w-md mx-auto'
-  >
-    <Card className='hover-interactive w-full'>
-      <CardHeader>
-        <CardTitle>{title}</CardTitle>
-        <CardDescription
-          className={cn(
-            'font-medium',
-            status === 'complete' && 'text-emerald-800',
-          )}
-        >
-          {humanizeStatus(status)}
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <RenderIf condition={!!score}>
-          <p className='text-sm'>
-            Score: <span className='font-mono tracking-tight'>{score}</span>
-          </p>
-        </RenderIf>
-        <RenderIf condition={!!totalTimeSpent}>
-          <p className='text-sm'>
-            Total Time: {humanizeDuration(totalTimeSpent ?? 0)}
-          </p>
-        </RenderIf>
-      </CardContent>
-    </Card>
-  </Link>
-);
+}) => {
+  // Render helpers ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+  const canRenderStatus =
+    status === 'not_yet_started' || status === 'in_progress';
+  const inProgress = status === 'in_progress';
+  // <~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Render helpers
+
+  return (
+    <Link
+      to={route}
+      params={{ userId, assignmentId: userAssignmentId.toString() }}
+      key={userAssignmentId}
+      className='w-full max-w-sm lg:max-w-md mx-auto'
+    >
+      <Card className='hover-interactive w-full'>
+        <CardHeader>
+          <CardTitle>{title}</CardTitle>
+          <RenderIf condition={canRenderStatus}>
+            <CardDescription
+              className={cn(
+                'font-semibold',
+                inProgress ? 'text-amber-700' : 'text-sky-800',
+              )}
+            >
+              {humanizeStatus(status)}
+              {inProgress && ': Paused'}
+            </CardDescription>
+          </RenderIf>
+        </CardHeader>
+        <CardContent>
+          <RenderIf condition={!!score}>
+            <p className='text-sm'>
+              Score: <span className='font-mono tracking-tight'>{score}</span>
+            </p>
+          </RenderIf>
+          <RenderIf condition={!!totalTimeSpent}>
+            <p className='text-sm'>
+              Total Time: {humanizeDuration(totalTimeSpent ?? 0)}
+            </p>
+          </RenderIf>
+        </CardContent>
+      </Card>
+    </Link>
+  );
+};
 
 export default AssignmentCard;
