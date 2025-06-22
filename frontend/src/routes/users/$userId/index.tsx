ce69e439d24f72ff6fd3bb21e8255e31111f1193
Benji Thompson <thompson.benji@gmail.com>
2025-06-22 15:02:25 -0400

    docs[FE]: add note on Route for /users/:userId/

--- frontend/src/routes/users/$userId/index.tsx
+++ frontend/src/routes/users/$userId/index.tsx
@@ -1,5 +1,8 @@
 import { createFileRoute, redirect } from '@tanstack/react-router';
 
+// This route could be used in the future for a more general user profile page
+// or dashboard, but currently redirects to the assignments page.
+
 export const Route = createFileRoute('/users/$userId/')({
   loader: ({ params }) => {
     throw redirect({
